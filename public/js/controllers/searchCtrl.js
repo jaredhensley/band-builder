a91e4a4bfba391952d60b864167216de94a68b33
Jared Hensley <jahensle85@me.com>
2015-12-17 15:59:03 -0600

    here

--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -3,6 +3,7 @@ angular.module('myApp').controller('searchCtrl', function ($scope, searchService
   $scope.findNearby = function (location) {
     searchService.findNearby(location).then(function (results) {
       $scope.locations = results.data;
+      console.log(results.data);
     });
   }
 
