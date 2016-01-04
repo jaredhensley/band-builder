13afa9c0b6145d56cc7c963571df23bb2dc53097
Jared Hensley <jahensle85@me.com>
2016-01-03 19:09:13 -0600

    fixed navbar state

--- public/js/controllers/mainctrl.client.controller.js
+++ public/js/controllers/mainctrl.client.controller.js
@@ -1,11 +1,22 @@
 angular.module('myApp').controller('MainCtrl', function ($state, $stateParams, $scope, UserService, IdentityService) {
 
+  $scope.loggedIn = window.user;
+
+  if ($scope.loggedIn) {
+    $scope.user = window.user;
+  }
+
   // setter function or a getter function ??
   $scope.updateUser = function () {
     window.user = IdentityService.currentUser;
     $scope.user = IdentityService.currentUser;
+    if ($scope.user) {
+      $scope.loggedIn = true;
+    } else {
+      $scope.loggedIn = false;
+    }
   }
 
-  $scope.user = IdentityService.currentUser;
+
 
 });
